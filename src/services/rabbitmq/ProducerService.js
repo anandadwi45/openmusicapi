283f65a24da7be0c6686b33efaf60373351f9aed
Ananda Dwi <anandadwi45@gmail.com>
2023-08-21 11:05:12 +0800

    update cache exp time

--- src/services/rabbitmq/ProducerService.js
+++ src/services/rabbitmq/ProducerService.js
@@ -3,7 +3,7 @@ const config = require('../../utils/config.js')
 
 const ProducerService = {
   sendMessage: async (queue, message) => {
-    const connection = await amqp.connect(config.rabbitMq.host)
+    const connection = await amqp.connect(config.rabbitMq.server)
     const channel = await connection.createChannel()
     await channel.assertQueue(queue, {
       durable: true
